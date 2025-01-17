de27d6d1d40f014b995a5ab4340adbea8a6876c1
Zoe <zmettauer@icloud.com>
2025-01-17 00:33:04 -0700

    start stats on site

--- celeste/main.js
+++ celeste/main.js
@@ -1,6 +1,6 @@
     const apiUrl = 'https://goldberries.net/api/player/stats'
     const id = 1784
-
+    
 fetch(apiUrl + "?id=" + id)
     .then(response => {
         if (!response.ok) {
@@ -10,7 +10,11 @@ fetch(apiUrl + "?id=" + id)
         })
     .then(data => {
         console.log(data)
+        document.getElementById("hardList").innerHTML = data.count_total_hard_list
+        document.getElementById("standardList").innerHTML = data.count_total_standard_list
     })
     .catch(error => {
         console.error('Error:', error);
+        document.getElementById("hardList").innerHTML = "Error loading"
+        document.getElementById("standardList").innerHTML = "Error Loading"
     })
